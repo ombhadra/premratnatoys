7725f75952e50a5d11a32421ebbee1427d040950
ombhadra <ombbh4@gmail.com>
2024-08-20 07:56:01 +0530

    initial

---
+++ custom.js
@@ -0,0 +1,71 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+// isotope js
+$(window).on('load', function () {
+    $('.filters_menu li').click(function () {
+        $('.filters_menu li').removeClass('active');
+        $(this).addClass('active');
+
+        var data = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: data
+        })
+    });
+
+    var $grid = $(".grid").isotope({
+        itemSelector: ".all",
+        percentPosition: false,
+        masonry: {
+            columnWidth: ".all"
+        }
+    })
+});
+
+// nice select
+$(document).ready(function() {
+    $('select').niceSelect();
+  });
+
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    <><iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=11%20HARICHAND%20COMPOUND%20NEAR%20GODREJ%20PETROL%20PUMP,%20Lal%20Bahadur%20Shastri%20Rd,%20Vikhroli%20West,%20Mumbai,%20Maharashtra%20400079%20Mumbai+(Prem%20ratna%20toys)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href='https://www.easybooking.eu/'>Hotelsoftware kleine Hotels</a><script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=4383e7334df0b37e699909030a7581603349c31c'></script></>
+}
+
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
